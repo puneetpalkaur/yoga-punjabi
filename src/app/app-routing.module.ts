947201e4c75c6844029d52ff210bdbcd2fbe8e93
puneetpalkaur <billingpuneet.13@gmail.com>
2020-04-14 07:23:31 -0400

    new routes asana and pranayama

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {PranayamaComponent} from './pranayama/pranayama.component';
+import {AsanaComponent} from './asana/asana.component';
+import {DashboardComponent} from './dashboard/dashboard.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/', pathMatch: 'full' },
+  { path: '', component: DashboardComponent },
+  { path: 'pranayama', component: PranayamaComponent },
+  { path: 'asana', component: AsanaComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,19 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { PranayamaComponent } from './pranayama/pranayama.component';
+import { AsanaComponent } from './asana/asana.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DashboardComponent
+    DashboardComponent,
+    PranayamaComponent,
+    AsanaComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
